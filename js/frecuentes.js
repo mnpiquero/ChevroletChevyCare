df36bb47cd36059c1c151fb829837ef708fce12f
mnpiquero <mpiquero@uade.edu.ar>
2024-11-10 20:53:03 -0300

    freceuntes

---
+++ js/frecuentes.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.faq-question').forEach(question => {
+    question.addEventListener('click', () => {
+        const answer = question.nextElementSibling;
+        const toggle = question.querySelector('.toggle');
+
+        if (answer.style.display === 'block') {
+            answer.style.display = 'none';
+            toggle.textContent = '+';
+        } else {
+            answer.style.display = 'block';
+            toggle.textContent = '-';
+        }
+    });
+});
